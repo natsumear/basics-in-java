2f5e4cda89af77c66fa688eeb8e677205807d7ee
natsumear <123586670+natsumear@users.noreply.github.com>
2023-01-27 03:48:02 +0530

    Update firstclass.java

--- firstclass.java
+++ firstclass.java
@@ -1,7 +1,7 @@
 class firstclass{
     public static void main(String[] args) {
         //how to use println and print;and \n; using line break.
-        System.out.print(" i am arghya and my friend is");
+        System.out.print(" i am natsumear and my friend is");
         System.out.print(" sayak\n he often treat me a briyani though\n okey its done\n");
     }
 }
