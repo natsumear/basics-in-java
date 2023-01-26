676e871e4b984f83f3f5605bbbf8b6da1fa1bc8a
natsumear <123586670+natsumear@users.noreply.github.com>
2023-01-27 02:57:26 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class gggg {
+public class sum {
     public static void main(String[] args) {
         //hi is me natsume nice to have you.
         Scanner sc = new Scanner(System.in);
