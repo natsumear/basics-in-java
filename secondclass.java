ce9da8faa211d591742c6febd74f818362b7e48b
natsumear <123586670+natsumear@users.noreply.github.com>
2023-01-27 03:46:12 +0530

    Add files via upload

---
+++ secondclass.java
@@ -0,0 +1,9 @@
+class secondclass {
+   public static void main(String[] args) {
+      // in here i dont use scanner class,do not want any varriable from user
+      int a = 10;
+      int b = 18;
+      int ans = a*b/a+b;
+      System.out.println(ans); // i am not use "" because using that i am printing ans text.
+   }
+}
